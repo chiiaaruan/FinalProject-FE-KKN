e6505572b936f0e6cbd3c7ec9793db90f1dbf6fe
chiiaaruan <chiakampus@gmail.com>
2022-06-16 13:11:55 +0700

    color change landing page

--- src/content/Landing.js
+++ src/content/Landing.js
@@ -3,7 +3,7 @@ import { Card, Carousel, Col, Container, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./../style/landing.css";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Foot from "../components/Footer";
 
 const Landing = () => {
   return (
@@ -13,28 +13,12 @@ const Landing = () => {
         <Carousel fade>
           <Carousel.Item>
             <img className="d-block w-100" src="./1.jpg" alt="First slide" />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img className="d-block w-100" src="./2.jpg" alt="Second slide" />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img className="d-block w-100" src="./3.jpg" alt="Third slide" />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
       </Container>
@@ -110,7 +94,7 @@ const Landing = () => {
         </Container>
       </div>
 
-      <Footer />
+      <Foot />
     </>
   );
 };
